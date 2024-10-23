cf25bd1a79a378d47d4d18dcd59f4c638a7e21cb
cnhannon <chris.n.hannon@gmail.com>
2024-10-24 00:57:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -523,29 +523,29 @@ elif page == "Lifestyle Factors & Stress 2":
 # --------LIFESTYLE AND WELLBEING ANALYSIS---------------
 elif page == "Lifestyle and Wellbeing Analysis":
         st.markdown(
-        """
-        <style>
-        /* Apply the background image to the entire app */
-        .stApp {
-            background: url('https://github.com/cnhannon/sleeeeeeeeep/raw/main/images/09.png');
-            background-size: cover;
-            background-repeat: no-repeat;
-            background-position: center;
-        }
-    
-        /* Optional: Customize the sidebar to have a slight transparency */
-        .css-1d391kg {
-            background: rgba(255, 255, 255, 0.1);  /* Adjust transparency */
-        }
-    
-        /* Optional: Customize the main content area background (if needed) */
-        .css-18e3th9 {
-            background-color: transparent;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
+            """
+            <style>
+            /* Apply the background image to the entire app */
+            .stApp {
+                background: url('https://github.com/cnhannon/sleeeeeeeeep/raw/main/images/09.png');
+                background-size: cover;
+                background-repeat: no-repeat;
+                background-position: center;
+            }
+        
+            /* Optional: Customize the sidebar to have a slight transparency */
+            .css-1d391kg {
+                background: rgba(255, 255, 255, 0.1);  /* Adjust transparency */
+            }
+        
+            /* Optional: Customize the main content area background (if needed) */
+            .css-18e3th9 {
+                background-color: transparent;
+            }
+            </style>
+            """,
+            unsafe_allow_html=True
+        )
     st.header("Lifestyle and Wellbeing Analysis")
     col1, col2, col3 = st.columns(3)
 
