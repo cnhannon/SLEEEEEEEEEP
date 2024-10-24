01d6e822260c36320b1747719af2453abb62c473
cnhannon <chris.n.hannon@gmail.com>
2024-10-24 15:35:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,8 +80,8 @@ if page == "Home":
         
         st.markdown(
             """
-            - <a href="https://www.kaggle.com/code/hexenmeiser/sleep-efficiency-dataset-eda-and-scoring" style="color:#be4cef;">Sleep Efficiency Dataset</a>
             - <a href="https://www.kaggle.com/datasets/uom190346a/sleep-health-and-lifestyle-dataset" style="color:#be4cef;">Sleep, Health, and Lifestyle Dataset</a>
+            - <a href="https://www.kaggle.com/code/hexenmeiser/sleep-efficiency-dataset-eda-and-scoring" style="color:#be4cef;">Sleep Efficiency Dataset</a>
             - <a href="https://www.kaggle.com/datasets/ydalat/lifestyle-and-wellbeing-data" style="color:#be4cef;">Lifestyle and Wellbeing Dataset</a>
             - <a href="https://catalog.data.gov/dataset/work-schedules-and-sleep-patterns-of-railroad-employees-dispatcher-background-survey" style="color:#be4cef;">Work Schedules and Sleep Patterns of Railroad Employees Dataset</a>
             """,
