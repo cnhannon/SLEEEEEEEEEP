73bcd60a9bdfe324b630a3e526db65856f011cab
cnhannon <chris.n.hannon@gmail.com>
2024-10-25 09:12:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -216,7 +216,7 @@ elif page == "Consumption Habits & Sleep Efficiency":
         """)
         # -------ADDING--------
     # Create three columns but only use col1 and col3
-    col1, col2, col2 = st.columns([1, 1])
+    col1, col2 = st.columns([1, 1])
     # Participant demographics scorecard in column 1
     with col1:
         number_of_participants = 452  # Total number of participants
